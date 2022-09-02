f295247289dfd3b4708dc97e6161ec000fda2898
pirminis <pirminis@gmail.com>
2022-09-02 18:43:22 +0300

    Try different loading approach

--- refresh-on-inactivity.user.js
+++ refresh-on-inactivity.user.js
@@ -1,14 +1,13 @@
 // ==UserScript==
 // @name         Refresh on inactivity
 // @namespace    https://github.com/pirminis/refresh-on-inactivity
-// @version      0.0.1
+// @version      0.0.2
 // @description  Automatically refresh the page when the user is idle
 // @author       pirminis
 // @updateURL    https://github.com/pirminis/refresh-on-inactivity/raw/master/refresh-on-inactivity.user.js
 // @downloadURL  https://github.com/pirminis/refresh-on-inactivity/raw/master/refresh-on-inactivity.user.js
 // @include      http://*
 // @include      https://*
+// @run-at       document-idle
 // ==/UserScript==
-document.addEventListener("DOMContentLoaded", function() {
-  console.log("test!");
-});
+console.log("test!");
