51a922c74b65d397ae68672d1bde5b9c61ddd678
pirminis <pirminis@gmail.com>
2022-09-02 18:37:42 +0300

    Add updateURL and downloadURL meta data

--- refresh-on-inactivity.user.js
+++ refresh-on-inactivity.user.js
@@ -4,6 +4,8 @@
 // @version      0.0.1
 // @description  Automatically refresh the page when the user is idle
 // @author       pirminis
+// @updateURL    https://github.com/pirminis/refresh-on-inactivity/raw/master/refresh-on-inactivity.user.js
+// @downloadURL  https://github.com/pirminis/refresh-on-inactivity/raw/master/refresh-on-inactivity.user.js
 // @include      http://*
 // @include      https://*
 // ==/UserScript==
