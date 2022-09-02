71d2edf5a4f9d5aa4d1c81c2de7fb52088b887a6
pirminis <pirminis@gmail.com>
2022-09-02 18:36:26 +0300

    Add initial debug script

---
+++ refresh-on-inactivity.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Refresh on inactivity
+// @namespace    https://github.com/pirminis/refresh-on-inactivity
+// @version      0.0.1
+// @description  Automatically refresh the page when the user is idle
+// @author       pirminis
+// @include      http://*
+// @include      https://*
+// ==/UserScript==
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("test!");
+});
